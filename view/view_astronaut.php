6a7fecd25fa70afd863a9fbbccd1e67f5d129349
GoYutoPro <Goyutoofficial@gmail.com>
2021-11-30 10:19:58 +0000

    master

--- view/view_astronaut.php
+++ view/view_astronaut.php
@@ -14,17 +14,39 @@
 include '../Connection.php';
 
 $sql = "SELECT * FROM astronaut";
-$result = mysqli_query($connection,$sql);
-print_r($result);
-if ($result) {
-    while ($row = mysqli_fetch_row($result)) {
-        // print($row[0]);
-        // print($row[1]);
-        // print($row[2]);
-        print "<tr><td>" . $row["astronaut_id"]. "</td><td>" . $row["name"] . "</td><td>"
-. $row["no_missions"]. "</td></tr>";
-    }
+
+$result = $connection->query($sql);
+
+
+
+if ($result->num_rows > 0) {
+
+  
+
+  while($row = $result->fetch_assoc()) {
+
+    echo "id: " . $row["astronaut_id"]. " - Name: " . $row["name"]. " " . $row["no_missions"]. "<br>";
+
+  }
+
+} else {
+
+  echo "0 results";
+
 }
+
+
+// $result = mysqli_query($connection,$sql);
+// print_r($result);
+// if ($result) {
+//     while ($row = mysqli_fetch_row($result)) {
+//         // print($row[0]);
+//         // print($row[1]);
+//         // print($row[2]);
+//         print "<tr><td>" . $row["astronaut_id"]. "</td><td>" . $row["name"] . "</td><td>"
+// . $row["no_missions"]. "</td></tr>";
+//     }
+// }
 
 // if(!mysqli_query($connection, $sql)){
 //     die("Error:".mysqli_error($connection));
