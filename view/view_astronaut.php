a5d7cebdbd0fc8ea45acf240d1ecee31289163ff
GoYutoPro <Goyutoofficial@gmail.com>
2021-11-30 09:33:05 +0000

    master

--- view/view_astronaut.php
+++ view/view_astronaut.php
@@ -2,21 +2,7 @@
 <html>
 <head>
 <title>Table with database</title>
-<style>
-table {
-border-collapse: collapse;
-width: 100%;
-color: #588c7e;
-font-family: monospace;
-font-size: 25px;
-text-align: left;
-}
-th {
-background-color: #588c7e;
-color: white;
-}
-tr:nth-child(even) {background-color: #f2f2f2}
-</style>
+
 </head>
 <body>
 <table>
@@ -25,38 +11,40 @@ tr:nth-child(even) {background-color: #f2f2f2}
 <th>Name</th>
 <th>Number of Missions</th>
 </tr>
+
 <?php
-define("DB_NAME", "Block_2");
-define ("DB_USER", "admin");
-define ("DB_HOST", "localhost");
-define ("DB_PASSWORD", "password");
+include '../Connection.php';
 
-$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
-// Check connection
-if ($connection -> connection_error) {
-die("Connection failed: " . $connection -> connection_error);
-}
-
-$sql = "SELECT astronaut_id, name, no_missions";
-$result = $connection->query($sql);
-if ($result->num_rows > 0) {
-// output data of each row
-while($row = $result->fetch_assoc()) {
-echo "<tr><td>" . $row["astronaut_id"]. "</td><td>" . $row["name"] . "</td><td>"
-. $row["no_missions"]. "</td></tr>";
-}
-echo "</table>";
-} else { echo "0 results"; }
-$conn->close();
+$sql = "SELECT * FROM astronaut";
+$result = mysqli_query($connection,$sql); 
+echo $result;
+// if ($result->num_rows > 0) {
+// while($row = $result->fetch_assoc()) {
+// echo "<tr><td>" . $row["astronaut_id"]. "</td><td>" . $row["name"] . "</td><td>"
+// . $row["no_missions"]. "</td></tr>";
+//     }
+// } 
+// else { echo "0 results"; }
 ?>
 </table>
-</body>
-</html>
-<html>
-<body>
-    
-   <a href="../index.php">
+  <a href="../index.php">
        <button> Go Back to the previous Page
    </button></a>
 </body>
 </html>
+
+<style>
+table {
+border-collapse: collapse;
+width: 80%;
+color: #588c7e;
+font-family: monospace;
+font-size: 20px;
+text-align: center;
+}
+th {
+background-color: #588c7e;
+color: white;
+}
+tr:nth-child(even) {background-color: #f2f2f2}
+</style>
--- view/view_attend.php
+++ view/view_attend.php
@@ -1,8 +1,35 @@
+<!DOCTYPE html>
 <html>
+<head>
+<title>Table with database</title>
+
+</head>
 <body>
-    
-   <a href="../index.php">
-       <button> Go Back to the previous Page
-   </button></a>
+<table>
+<tr>
+<th>Missions_ID</th>
+<th>Number of Missions</th>
+</tr>
 </body>
 </html>
+
+
+
+
+
+
+<!-- <style>
+table {
+border-collapse: collapse;
+width: 80%;
+color: #588c7e;
+font-family: monospace;
+font-size: 20px;
+text-align: center;
+}
+th {
+background-color: #588c7e;
+color: white;
+}
+tr:nth-child(even) {background-color: #f2f2f2}
+</style> -->
