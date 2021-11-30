1892b42fd8bdc517d0f8a6ee99faef9130fae5ef
GoYutoPro <Goyutoofficial@gmail.com>
2021-11-30 10:13:33 +0000

    master

--- view/view_astronaut.php
+++ view/view_astronaut.php
@@ -21,8 +21,8 @@ if ($result) {
         // print($row[0]);
         // print($row[1]);
         // print($row[2]);
-        print "<tr><td>" . $row["0"]. "</td><td>" . $row["1"] . "</td><td>"
-. $row["2"]. "</td></tr>";
+        print "<td><td>" . $row["0"]. "</td><td>" . $row["1"] . "</td><td>"
+. $row["2"]. "</td></td>";
     }
 }
 
